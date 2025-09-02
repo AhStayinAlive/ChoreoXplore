382c32cb5c23b36b48166db81cad41e1331b6ff8
Ken Cheng <87518459+Kitch05@users.noreply.github.com>
2025-09-03 01:34:08 +0800

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.jsx'
+import ToggleKeyword from './reusables/toggle_keyword.jsx'
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
     <App />
+    <ToggleKeyword title='Hello'/>
+    <ToggleKeyword title='Hello'/>
+    <ToggleKeyword title='Hello'/>
   </StrictMode>,
 )
